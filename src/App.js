4e8eaf317edb81f84d924ef27a807633dce6c1b4
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-06-02 21:20:25 +0200

    made header and cards w styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from 'componentst/Header'
+import Header from 'components/Header'
+import TinderCards from './components/TinderCards/TinderCards';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <TinderCards />
     </div>
   )
 }
