90b191747983e72745967875a8641ec8d5eef051
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-06-04 22:04:22 +0200

    made routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+/* eslint-disable react/jsx-no-undef */
 import React from 'react'
 import Header from 'components/Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import TinderCards from './components/TinderCards/TinderCards';
+import Registration from './pages/Registration'
+/* import HomePage from 'pages/HomePage'; */
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <TinderCards />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/registration" element={<Registration />} /> {/* Add this route for Registration */}
+        <Route path="/tinder-cards" element={<TinderCards />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
