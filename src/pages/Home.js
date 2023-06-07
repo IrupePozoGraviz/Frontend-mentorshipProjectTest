80b150bd442ee2bb13f4bba1af573bea1448ea06
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-06-07 17:04:14 +0200

    new name from auth to login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@
 import { React, useState } from 'react'
 import { useCookies } from 'react-cookie' // install by running npm install react-cookie
 import Nav from '../components/Nav'
-import AuthModal from '../components/AuthModal'
+import AuthModal from '../components/login'
 
 const Home = () => {
   const [showModal, setShowModal] = useState(false)
