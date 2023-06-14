6350a62858449729ab8de32af691629da07306d5
Annika Lindberg <annika.edit.lindberg@gmail.com>
2023-06-14 11:02:27 +0200

    ..

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 /* eslint-disable react/no-array-index-key */
@@ -93,6 +94,31 @@ export const Dashboard = () => {
     console.log(`${myIdentifier}  left the screen`)
   }
 
+  // const fakeLoggedinUserId = '64772d4207f2d41ce2062496'
+  const handleLikePerson = (likePersonUserId) => {
+    fetch(API_URL(`/likedPersons/${userId}`), {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: accessToken
+      },
+      body: JSON.stringify({
+        likedUserId: likePersonUserId
+      })
+    })
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.success) {
+          // om ni vill navigera
+        } else {
+          console.error('Failed to save liked person')
+        }
+      })
+      .catch((error) => {
+        console.error('Failed to save liked Person', error)
+      })
+  }
+
   return (
     <main className="dashboard">
 
@@ -101,78 +127,82 @@ export const Dashboard = () => {
         <div className="profile-header">
           <h2>Dashboard</h2>
           <p> Dashboard/landnindssida typ: liked persons should be displayed for user in a
-        page together with more info Landningssida, där det typ står hej
-        mentor/mentee du har 100 mentorer väntande börja välj(decide on which component!).
-        Här finns en icon som vid klick tar användaren till "cards-att likea"-sidan.
+            page together with more info Landningssida, där det typ står hej
+            mentor/mentee du har 100 mentorer väntande börja välj(decide on which component!).
+            Här finns en icon som vid klick tar användaren till "cards-att likea"-sidan.
           </p>
           <h1>{`${currentUser.username}'s Profile`}</h1>
           <p>{`role: ${currentUser.role}`}</p>
         </div>
-        {loading ? 'loading...' : <div><h1>Get list of mentors/mentees here</h1>{matchingList.map((user) => <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}><UserCard user={user} /></TinderCard>)}</div>}
+        {loading ? 'loading...' : <div><h1>Get list of mentors/mentees here</h1>{matchingList.map((user) => <TinderCard onSwipe={onSwipe} onCardLeftScreen={() => onCardLeftScreen('fooBar')} preventSwipe={['right', 'left']}>
+          <div className="swipe-container">
+            <div className="card-container">
+              <div className="kort">
+                <img
+                  src="https://placebear.com/200/300"
+                  alt="Profile"
+                  style={{ width: 200, height: 300 }} />
+                <p> Mentor card</p>
+                <p> Picture of person </p>
+                <p>{user.username}</p>
+                <p>{user.role}</p>
+                <p> preferences: </p>
+                <p> info about our selfs </p>
+                <p> Emojis to show extra </p>
+                {/* {user.preferences.map((pref) => <p>{pref}</p>)} */}
+              </div>
+              <button
+                type="button"
+                onClick={() => handleLikePerson(user.id)}>Accept
+              </button>
+              <button
+                type="submit">
+                Decline
+              </button>
+            </div>
+          </div>
+        </TinderCard>)}
+        </div>}
       </div>
 
     </main>
   );
 };
 
-// make a onclick event that for a "like-button" that triggers a function that adds the user to the matchlist in the database by sending a PUT/PATCH request to the backend that updates the matchlist for the user in the database (in an array in the database) like this:
-// make a onclick event that for a "dislike-button" that triggers a function that adds the user to the matchlist in the database by sending a PUT/PATCH request to the backend that updates the matchlist for the user in the database (in an array in the database)
-const fakeLoggedinUserId = '64772d4207f2d41ce2062496'
-
-const handleLikePerson = (userId) => {
-  fetch(API_URL(`/likedPersons/${fakeLoggedinUserId}`), {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json'
-      // Authorization: accessToken
-    },
-    body: JSON.stringify({
-      likedUserId: userId
-    })
-  })
-    .then((res) => res.json())
-    .then((json) => {
-      if (json.success) {
-        // om ni vill navigera
-      } else {
-        console.error('Failed to save liked person')
-      }
-    })
-    .catch((error) => {
-      console.error('Failed to save liked Person', error)
-    })
-}
 // style det här! // behvöer en like knapp som skickar med user-id för det kortet som triggar en PUT/PATCH i backendet som uppdaterar matchen i databasen för den användaren och lägger till den i matchlistan för den användaren (i en array i databasen)
 // behöver en dislike knapp som triggar en PUT/PATCH i backendet som uppdaterar matchen i databasen för den användaren och lägger till den i matchlistan för den användaren (i en array i databasen)
 
-const UserCard = ({ user }) => {
-  console.log('user', user)
-  return (
-    <div className="swipe-container">
-      <div className="card-container">
-        <div className="kort">
-          <img
-            src="https://placebear.com/200/300"
-            alt="Profile"
-            style={{ width: 200, height: 300 }} />
-          <p> Mentor card</p>
-          <p> Picture of person </p>
-          <p>{user.username}</p>
-          <p>{user.role}</p>
-          <p> preferences: </p>
-          <p> info about our selfs </p>
-          <p> Emojis to show extra </p>
-          {/* {user.preferences.map((pref) => <p>{pref}</p>)} */}
-        </div>
-        <button
-          type="button"
-          onClick={() => handleLikePerson(user.id)}>Accept
-        </button>
-        <button
-          type="submit">
-          Decline
-        </button>
-      </div>
-    </div>
-  )
-}
+// make a onclick event that for a "like-button" that triggers a function that adds the user to the matchlist in the database by sending a PUT/PATCH request to the backend that updates the matchlist for the user in the database (in an array in the database) like this:
+// make a onclick event that for a "dislike-button" that triggers a function that adds the user to the matchlist in the database by sending a PUT/PATCH request to the backend that updates the matchlist for the user in the database (in an array in the database)
+
+// const UserCard = ({ user }) => {
+//   console.log('user', user)
+//   return (
+//     <div className="swipe-container">
+//       <div className="card-container">
+//         <div className="kort">
+//           <img
+//             src="https://placebear.com/200/300"
+//             alt="Profile"
+//             style={{ width: 200, height: 300 }} />
+//           <p> Mentor card</p>
+//           <p> Picture of person </p>
+//           <p>{user.username}</p>
+//           <p>{user.role}</p>
+//           <p> preferences: </p>
+//           <p> info about our selfs </p>
+//           <p> Emojis to show extra </p>
+//           {/* {user.preferences.map((pref) => <p>{pref}</p>)} */}
+//         </div>
+//         <button
+//           type="button"
+//           onClick={() => handleLikePerson(user.id)}>Accept
+//         </button>
+//         <button
+//           type="submit">
+//           Decline
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
